2030b4132842220aef906d8fcea2457bfc3e4e8b
Dylan Kirdahy <accounts@dylankirdah.nyc>
2026-03-16 18:34:01 -0400

    added drive mounts, added syncthing config

--- hosts/diy-nas/default.nix
+++ hosts/diy-nas/default.nix
@@ -27,5 +27,40 @@
 	syncthing
   ];
 
+  services.syncthing = {
+  enable = true;
+  user = "dylan";
+  dataDir = "/home/dylan/pool/syncthing";
+  guiAddress = "0.0.0.0:8384";
+};
+
+networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+
+  fileSystems."/home/dylan/pool/nextcloud" = {
+  device = "/dev/disk/by-uuid/acb218f0-ce2d-4d6e-8fa6-43c1e1c01470";
+  fsType = "btrfs";
+  options = [ "subvol=@nextcloud" "compress=zstd" "noatime" ];
+};
+
+  fileSystems."/home/dylan/pool/syncthing" = {
+  device = "/dev/disk/by-uuid/acb218f0-ce2d-4d6e-8fa6-43c1e1c01470";
+  fsType = "btrfs";
+  options = [ "subvol=@syncthing" "compress=zstd" "noatime" ];
+};
+
+fileSystems."/home/dylan/pool/backups" = {
+  device = "/dev/disk/by-uuid/acb218f0-ce2d-4d6e-8fa6-43c1e1c01470";
+  fsType = "btrfs";
+  options = [ "subvol=@backups" "compress=zstd" "noatime" ];
+};
+
+fileSystems."/home/dylan/pool/network-storage" = {
+  device = "/dev/disk/by-uuid/acb218f0-ce2d-4d6e-8fa6-43c1e1c01470";
+  fsType = "btrfs";
+  options = [ "subvol=@network-storage" "compress=zstd" "noatime" ];
+};
+
+
   system.stateVersion = "25.11";
 }
